6d119c59a743047dc093add6852b8d3f360066ca
navvwd <162856796+navvwd@users.noreply.github.com>
2025-07-03 01:35:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,21 @@
-// server.js (Node.js backend)
+
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const app = express();
 
-// Middleware
 app.use(bodyParser.json());
-app.use(express.static('public')); // Serve static files from 'public' directory
+app.use(express.static('public')); y
 
-// Connect to MongoDB
 mongoose.connect('mongodb://localhost:27017/take2media', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.error(err));
+// mongo data(schema ony)
 
-// Enquiry Schema
 const enquirySchema = new mongoose.Schema({
     name: String,
     email: String,
@@ -26,8 +24,7 @@ const enquirySchema = new mongoose.Schema({
     date: { type: Date, default: Date.now }
 });
 const Enquiry = mongoose.model('Enquiry', enquirySchema);
-
-// Route to handle contact form submission
+//req
 app.post('/enquiry', (req, res) => {
     const { name, email,phone_no, message } = req.body;
     const newEnquiry = new Enquiry({ name, email,phone_no, message});
@@ -35,8 +32,6 @@ app.post('/enquiry', (req, res) => {
         .then(() => res.status(200).send("Saved"))
         .catch(err => res.status(500).json({ error: err }));
 });
-
-// Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
